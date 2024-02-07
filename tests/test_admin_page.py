4f0c0c84d3067784e766e1c2195614b18f16314a
Karthik <tpurposing@gmail.com>
2024-02-07 11:31:34 +0530

    implemented explicit and implicit waits and added common explicit wait method

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -18,6 +18,8 @@ class TestAdminPage(BaseClass):
         # Navigating to admin page from HRM home page
         obj_admin_page.goto_admin_tab().click()
 
+        self.explicit_wait_hrm(5, obj_admin_page.admin_page_validation)
+
         # Validating navigation to admin page
         admin_page_validation_text = obj_admin_page.validating_admin_page().text
         assert admin_page_validation_text == 'System Users', 'Admin page validation is FAILED'
@@ -25,7 +27,8 @@ class TestAdminPage(BaseClass):
 
         # Navigating to system user add page from admin page
         obj_admin_page.goto_system_user_add_page().click()
-        time.sleep(1)
+
+        self.explicit_wait_hrm(5, obj_admin_page.system_user_add_page_validation)
 
         # Validating navigation to system user add page
         system_user_add_page_validation_text = obj_admin_page.validating_system_user_add_page().text
@@ -34,44 +37,41 @@ class TestAdminPage(BaseClass):
 
         # To expand system user role dropdown
         obj_admin_page.expanding_user_role_dropdown().click()
-        time.sleep(1)
+
+        self.explicit_wait_hrm(5, obj_admin_page.user_dropdown_select_admin)
 
         # Selecting admin user role from system user role dropdown
         obj_admin_page.selecting_admin_userrole().click()
-        time.sleep(1)
 
         # To expand system user status dropdown
         obj_admin_page.expanding_user_status_dropdown().click()
-        time.sleep(1)
+
+        self.explicit_wait_hrm(3, obj_admin_page.user_status_select_enabled)
 
         # Selecting enabled user status from system user status dropdown
         obj_admin_page.selecting_enabled_system_status().click()
-        time.sleep(1)
 
         # Adding new employee name to add system user
-        obj_admin_page.add_employee_name().send_keys("Coll")
-        time.sleep(2)
+        obj_admin_page.add_employee_name().send_keys("Russ")
+
+        self.explicit_wait_hrm(3, obj_admin_page.employee_name_select)
 
         # Selecting employee name from employee name hints
         obj_admin_page.selecting_employee_name().click()
-        time.sleep(2)
 
         # Adding username to add system user username field
-        obj_admin_page.add_system_user_username().send_keys("kktestadmin")
-        time.sleep(1)
+        obj_admin_page.add_system_user_username().send_keys("kktestadmins")
 
         # Adding password to system user password field
-        obj_admin_page.add_system_user_password().send_keys("kktestadmin1")
-        time.sleep(1)
+        obj_admin_page.add_system_user_password().send_keys("kktestadmins1")
 
         # Adding confirm password to system user confirm password field
-        obj_admin_page.add_system_user_confirm_password().send_keys("kktestadmin1")
-        time.sleep(1)
+        obj_admin_page.add_system_user_confirm_password().send_keys("kktestadmins1")
 
         # Save new system user
         obj_admin_page.save_system_user().click()
 
-        time.sleep(1)
+        self.explicit_wait_hrm(3, obj_admin_page.system_user_adduser_success_validation)
 
         add_system_user_success_message_text = obj_admin_page.validating_system_user_adduser_success_message().text
         assert add_system_user_success_message_text == 'Successfully Saved', 'Add System user success validation FAILED.'
